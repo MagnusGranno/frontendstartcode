e25b8a060b16d955c90c5662eed654506a84cebe
Alek0329 <alek0329@gmail.com>
2021-11-12 17:05:03 +0100

    fixed login

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 //Base URL
-export const myUrl = 'http://localhost:8080/startcode_security_war_exploded';
+export const myUrl = 'http://localhost:8080/security_start_war_exploded';
 
 export const fiveServers = '/api/info/fiveServers';
 
